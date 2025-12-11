2f3b5ea9dc9c6d23d9451b17e98a6a4db3b6056e
Ben Graham <grahamben7@gmail.com>
2025-12-11 08:31:10 +0200

    basic routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { ProjectPage } from './pages/project-page/project-page';
+import { AboutPage } from './pages/about-page/about-page';
+import { ProductPage } from './pages/product-page/product-page';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'projects',
+        component: ProjectPage
+    },
+    {
+        path: 'products',
+        component: ProductPage
+    },
+    {
+        path: 'about',
+        component: AboutPage
+    }
+];
